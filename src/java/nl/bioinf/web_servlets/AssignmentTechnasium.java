87d952de6d5bae47cefe2b01d4fb1d60986266f0
Mariska Slofstra <mariska-karina@hotmail.com>
2014-12-14 21:15:42 +0100

    servlet toegevoegd

---
+++ src/java/nl/bioinf/web_servlets/AssignmentTechnasium.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.bioinf.web_servlets;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author mldubbelaar
+ */
+public class AssignmentTechnasium extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @SuppressWarnings("empty-statement")
+    protected void processRequest(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+
+        String notesOnSuspect;
+
+        notesOnSuspect = request.getParameter("notes");
+        try {
+            //File fileDir = new File("/homes/mldubbelaar/Desktop/data.txt");
+            File fileDir = new File("/Users/mldubbelaar/Desktop/data.txt");
+            try (Writer output = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(fileDir), "UTF8"))) {
+                output.append(notesOnSuspect);
+                output.flush();
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+
+        RequestDispatcher view = request.getRequestDispatcher(
+                "html/technasiumWorkshop.jsp");
+        view.forward(request, response);
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
