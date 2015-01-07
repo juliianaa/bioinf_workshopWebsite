c14c207ebf77cd4ded30b0025fe43c2b0e39b68c
aroeters <aroeters@bin259.bin>
2015-01-07 12:19:01 +0100

    bezig met ajax mbo en masterclass

--- web/js/navigateQuestions.js
+++ web/js/navigateQuestions.js
@@ -3,20 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-alert("hi");
-$(document).ready(initialize);
+jQuery(document).ready(initialize);
 
 function initialize() {
-    $("#next_question_button").click(function () {
-        var pageID = $("#refreshQuestion").data("nr");
-        alert(pageID);
+    jQuery("#next_question_button").click(function () {
+        var pageID = jQuery("#number_passer").data("nr");
         switch (pageID) {
             case 1:
-                $("#question").load("html/assignmentsMasterClassNHanzexperience/assignment2.jsp");
+                jQuery("#question").load("html/assignmentsMasterClassNHanzexperience/assignment1bc.jsp");
                 break;
             case 2:
-                $("#question").load("html/assignmentsMasterClassNHanzexperience/assignment3.jsp");
+                jQuery("#question").load("html/assignmentsMasterClassNHanzexperience/assignment2a.jsp");
                 break;
+            case 3:
+                jQuery("#question").load("html/assignmentsMasterClassNHanzexperience/assignment2b.jsp");
+                break;
+            case 4:
+                jQuery("#question").load("html/assignmentsMasterClassNHanzexperience/assignment2c.jsp");
+                break;
+
         }
 
     });
