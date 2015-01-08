fe01bbe63d9d897531580e5878dba503fe499959
aroeters <aroeters@bin258.bin>
2015-01-08 15:24:16 +0100

    typetext altered

--- web/js/typeText.js
+++ web/js/typeText.js
@@ -16,7 +16,7 @@ $(function(){
             "• Je kunt beleven hoe het is om een specifieke opleiding \nop hbo-niveau te volgen",
             "• Je krijgt informatie over verschillende opleidingen en de\nberoepen die daarbij horen",
             "We hopen dat je veel plezier zult beleven aan dit driedaagse evenement en \ndat het je kan ondersteunen in je keuze of je gaat doorsturen in het hbo."],
-        typeSpeed: 0,
+        typeSpeed: -10,
         backDelay: 800,
         loop: false,
         loopCount: false
@@ -25,7 +25,7 @@ $(function(){
     $("#login_text").typed({
         strings: ["Voordat we beginnen, geef jezelf eerst een username voor dit \ndriedaagse workshop", 
             "<form action='answer.do' method='post'>username: <input type='text' name='username'></input><input class='button' type='submit' value='Submit'></input></form>"],
-        typeSpeed: 0,
+        typeSpeed: -10,
         backDelay: 800,
         loop: false,
         loopCount: false,
