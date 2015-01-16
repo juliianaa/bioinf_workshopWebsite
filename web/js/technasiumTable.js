a7075fbe335755a8350a9c4298a1cb3b1718ad87
Mariska <Mariska@Mariska-PC>
2015-01-16 22:15:03 +0100

    checkboxes save when checked

--- web/js/technasiumTable.js
+++ web/js/technasiumTable.js
@@ -1,27 +1,32 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Made by Mkslofstra
+ * purpose: to save the content of the table on the technasium page. 
  */
 
 $(document).ready(initialize);
 function initialize() {
-    var checked = [];
-    $("#saveTable").click(function () {
-        var checked = [];
-        $('input[type=checkbox]').each(function () {
-            if (this.checked) {
-                var checkId = this.id;
-                checked.push(checkId);
-            }
-            $.removeCookie('checked');
-            $.cookie('checked', checked, {path: '/'});
-            console.log($.cookie('checked'));
-        });
-        $("#getTable").click(function () {
-            var koekje = $.cookie('checked');
-            alert(koekje);
-        });
+    // load the previous answered information
+    // walk through each cookie
+    for (var id in $.cookie()){
+        //save the value of the cookie (this is true or false, whether the box was checked or not).
+        var value = $.cookie(id);
+        //if the value is true, check the box again on this page. 
+        if(value === true|value ==="true"){
+            $("#"+id).prop('checked', true);
+        }
+        //if the value is false, uncheck the box again on this page. 
+        else if(value === false | value === "false"){
+            $("#"+id).prop('checked', false);
+        }
+    }
+    // is a checkbox is clicked, save if its checked or unchecked. 
+    $('.check').click(function () {
+        // if the click checks the box, set the value in the cookie with this id true
+        if (this.checked) {
+            $.cookie($(this).attr('id'), true);
+        // else (box unchecked), set the value in the cookie with this id false. 
+        }else{
+             $.cookie($(this).attr('id'), false);
+        }
     });
-
 }
