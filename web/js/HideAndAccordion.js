1b394f1e25484364ec279df07eecb1ef3948872b
macbook <macbook@145.37.90.131>
2015-01-21 12:11:19 +0100

    Worked on the KankerCasus for workshops. Hide and show added. Only the last page still needs to be finished.

--- web/js/HideAndAccordion.js
+++ web/js/HideAndAccordion.js
@@ -6,4 +6,37 @@
 $(function() {
     $( "#accordion" ).accordion();
   });
-  
+
+  $(function() {
+    // run the currently selected effect
+    function runEffect() {
+      // get effect type from
+      var selectedEffect = $( "#effectTypes" ).val();
+ 
+      // most effect types need no options passed by default
+      var options = {};
+      // some effects have required parameters
+      if ( selectedEffect === "scale" ) {
+        options = { percent: 100 };
+      } else if ( selectedEffect === "size" ) {
+        options = { to: { width: 850, height: 1015 } };
+      }
+ 
+      // run the effect
+      $( "#effect" ).show( selectedEffect, options, 500, callback );
+    };
+ 
+    //callback function to bring a hidden box back
+    function callback() {
+      setTimeout(function() {
+        $( "#effect:visible" ).removeAttr( "style" ).fadeOut();
+      }, 100000000 );
+    };
+ 
+    // set effect from select menu value
+    $( "#button" ).click(function() {
+      runEffect();
+    });
+ 
+    $( "#effect" ).hide();
+  });
